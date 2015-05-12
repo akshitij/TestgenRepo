6d99c856bda511de95a00b95438574a9294751aa
akshitij <kshitij.ashwini@gmail.com>
2015-05-13 01:58:59 +0530

    Coverage will be reported of all conditions of file due to add_condition of all funtions

--- Testgen/loopTest.c
+++ Testgen/loopTest.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+int foo(int x){
+	int y;
+	if(x>2)
+		y=2;
+	else
+		y=4;
+	/*for(;x<5;x++){
+		y = x+1;
+	}*/
+	return y;
+}
+
 int main(){
-	int i,j;
+	int i;
 	scanf("%d",&i);
 	for(;i<8;i++){
-		j = i+2;
+		i = i+2;
 	}
 	//foo(2);
 	return 0;
